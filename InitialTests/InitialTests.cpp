3000e3660c9c59942f5c36f1793031e3e000d8b0
Matheus Santos <matheusgabriel.18@gmail.com>
2020-09-28 22:53:55 -0300

    linearSearch pronto (tempo no pior caso 300ns)

--- InitialTests/InitialTests.cpp
+++ InitialTests/InitialTests.cpp
@@ -6,7 +6,8 @@
 #define TABLE_SIZE 360
 #define MILLION 1000000
 
-unsigned int tgTable[TABLE_SIZE][2];										//array de array onde o array de 2 posições é: [0] = x; [1] = tg(x)
+//array de array onde o array de 2 posições é: [0] = x; [1] = tg(x)
+unsigned int tgTable[TABLE_SIZE][2];
 
 float linearSearch(float tg);
 
@@ -20,26 +21,28 @@ int main()
 	teria que guardar 360 valores. Como é uma tabela dupla 360 x 2 = 720 valores.
 	*/
 
-	unsigned int x = 0;														//variável auxiliar
+	//passo de construção da tabela onde a precisão é 0.25 mas é multiplicado por 100 para virar int, assim aumenta 25 a cada ciclo
+	unsigned int x = 0;
+
 	for (short i = 0; i < TABLE_SIZE; i++)
 	{	
-		if (i == 0)															//tg(0) = 0
+		if (i == 0)																//tg(0) = 0
 		{
-			tgTable[i][0] = x;												//valor de x
-			tgTable[i][1] = 0;												//valor de tg(x)
-			x = x + 25;														//passo
+			tgTable[i][0] = 0;													//valor de x
+			tgTable[i][1] = 0;													//valor de tg(x)
+			x = x + 25;															//passo
 		}
 		else
 		{
-			tgTable[i][0] = x;												//valor de x
-			tgTable[i][1] = tan((x / 100.0) * (PI / 180.0)) * MILLION;		//valor de tg(x), onde (PI / 180.0) é para transformar de grau para rad
-			x = x + 25;														//passo
+			tgTable[i][0] = x;													//valor de x
+			tgTable[i][1] = tan((x / 100.0) * (PI / 180.0)) * MILLION;			//valor de tg(x), (PI / 180.0) é para transformar de grau para rad e * MILLION é para transformar de float para int com precisão de 6 casas
+			x = x + 25;															//passo
 		}
 	}	
 	
 	auto inicio = std::chrono::high_resolution_clock::now();
 
-	float linear = linearSearch(0.527686f);
+	float linear = linearSearch(100.0f);
 
 	auto resultado = std::chrono::high_resolution_clock::now() - inicio;
 	long long nanoseconds = std::chrono::duration_cast<std::chrono::nanoseconds>(resultado).count();
@@ -49,50 +52,33 @@ int main()
 
 float linearSearch(float tg)
 {
-	bool isNegative = tg < 0;												//preciso dela já que reduzi o número de valores pelo fato de tg(x) ser ímpar
-	std::cout << "isNegative: " << isNegative << std::endl;
-	unsigned int tgInt = isNegative ? tg * MILLION * (-1) : tg * MILLION;
-	std::cout << "tgInt: " << tgInt << std::endl;
-	unsigned int atgInt = 0;
+	bool isNegative = tg < 0;													//preciso dela já que reduzi o número de valores pelo fato de tg(x) ser ímpar
+	unsigned int tgInt = isNegative ? tg * MILLION * (-1) : tg * MILLION;		//transformando o float tg no int tgInt para melhor precisão na comparação
+	unsigned int atgInt = 0;													//arco tangente relativo à tgInt, em graus °
 
-	if (tgInt > tgTable[TABLE_SIZE - 1][1])									//aqui é onde o valor fica próxima da assíntota e assume o valor máximo da tabela, 89.75°
+	if (tgInt > tgTable[TABLE_SIZE - 1][1])										//aqui é onde o valor fica próxima da assíntota e assume o valor máximo da tabela, 89.75°
 	{
-		std::cout << "Maior que 8975!!" << std::endl;
-		atgInt = tgTable[TABLE_SIZE - 1][0];								//valor em grau, depois será convertido para rad
+		atgInt = tgTable[TABLE_SIZE - 1][0];
 	}
 	else
 	{
 		for (short i = 0; i < TABLE_SIZE - 1; i++)
 		{
 			/*
-			Aqui é o seguinte: o valor de tg(x) é comparado com os valores da tabela, se ele for maior que o do ciclo atual e menor que o do próximo então
+			Aqui é o seguinte: o valor de tgInt é comparado com os valores da tabela, se ele for maior que o do ciclo atual e menor que o do próximo então
 			ele está neste intervalo. Se verdadeiro: Para achar o valor aproximado mais preciso tem a variável supDif (diferença superior) que é a diferença
-			entre o valor de teste e o do proximo ciclo da tabela, e a variável infDiff (diferença inferior) que é a diferença entre o valor da tabela do
-			ciclo atual e o valor de teste. Se infDiff < supDiff então o mais preciso é usar o valor da tabela do ciclo atual, se falso então usa-se o valor
-			da tabela do próximo ciclo. Ex.: se value fosse 8, ciclo atual 5 e próximo fosse 10; então supDiff = 10-8 = 2 e infDiff = 8-5 = 3, sendo assim o
+			entre o valor de tgInt e o do proximo ciclo da tabela, e a variável infDiff (diferença inferior) que é a diferença entre o valor da tabela do
+			ciclo atual e o valor de tgInt. Se infDiff < supDiff então o mais preciso é usar o valor da tabela do ciclo atual, se falso então usa-se o valor
+			da tabela do próximo ciclo. Ex.: se tgInt fosse 8, ciclo atual 5 e próximo fosse 10; então supDiff = 10-8 = 2 e infDiff = 8-5 = 3, sendo assim o
 			valor aproximado mais correto seria o 10 por estar mais próximo de 8 do que o 5.
 			*/
-			
-			std::cout << "============== " << i << " ==============" << std::endl;
-			std::cout << "Ciclo atual: tg(x) = " << tgTable[i][1] << "\tx = " << tgTable[i][0] << std::endl;
-			std::cout << "Proximo ciclo: tg(x) = " << tgTable[i + 1][1] << "\tx = " << tgTable[i + 1][0] << std::endl;
 
 			if (tgInt >= tgTable[i][1] && tgInt <= tgTable[i + 1][1])
-			{				
+			{
 				unsigned int supDif = tgTable[i + 1][1] - tgInt;
-				std::cout << "supDif: " << supDif << std::endl;
 				unsigned int infDif = tgInt - tgTable[i][1];
-				std::cout << "infDif: " << infDif << std::endl;
 
-				if (supDif > infDif)
-				{
-					atgInt = tgTable[i][0];									//valor em grau, depois será convertido para rad
-				}
-				else
-				{
-					atgInt = tgTable[i + 1][0];								//valor em grau, depois será convertido para rad
-				}
-				std::cout << "atgInt: " << atgInt << std::endl;
+				atgInt = (supDif > infDif) ? atgInt = tgTable[i][0] : atgInt = tgTable[i + 1][0];
 				break;
 			}
 		}
@@ -101,9 +87,10 @@ float linearSearch(float tg)
 	if (isNegative)
 	{
 		//Peculiaridade da função tg(x) por ser ímpar
+		//9000 é 90° x 100 porque eu transformei os ângulos de float para int multiplicando por 100
 		atgInt = 9000 + (9000 - atgInt);
 	}
 
 	//convertendo o valor para rad
-	return atgInt;
+	return atgInt / 100.0;
 }
